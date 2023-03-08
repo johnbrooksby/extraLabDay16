054a262d79e70b77143cbfffbff4fe192c0f0eb0
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-07 23:49:44 -0700

    Extra challenge progress commit

---
+++ client/foodOrder.js
@@ -0,0 +1,7 @@
+let formSubmit = document.querySelector("button");
+let form = document.querySelector(".form-layout");
+
+function addOrder(event) {
+    event.preventDefault();
+    axios.push()
+}
